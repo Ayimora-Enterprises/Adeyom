84e22bd6a0ce8f241765365e324370a89c656f4e
abayomia2015 <107432052+abayomia2015@users.noreply.github.com>
2022-06-17 00:16:54 -0500

    Update kalegreens.sh
    good enough

--- kalegreens.sh
+++ kalegreens.sh
@@ -3,3 +3,7 @@ l halleluyah is the morning of the Gos of Isreal
 yea!!!!!
 YEA!!!!!!!
 Halleluyah!!!!
+jjkmdkgoerkgeropkgperkgpergpergpelgp[oler
+dfngpekrgoerf[ekrfg[pkerfgp[er
+kerjgiwerjfpwejfowepjfwep
+keroigfipejwpofjkwoefkwe

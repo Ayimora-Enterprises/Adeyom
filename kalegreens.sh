17a3bf0a97a92c26ee7f6bbebe9bc97cf70c097a
abayomia2015 <107432052+abayomia2015@users.noreply.github.com>
2022-06-17 00:19:36 -0500

    Update kalegreens.sh

--- kalegreens.sh
+++ kalegreens.sh
@@ -1,2 +1,8 @@
 nvopjfo[wjfpkwepfkw]epkf[\welf]\welf[wekjfopwenfkjwebfpwejfpkqw[flkw\[e
-l
+lfgho[kpl][lp[lp[lplpkopkokoetkgh=perklpgole]r]erpg
+erkgjoperjg[oekrpg[ke[rg
+ergjeorpkg[erkger
+ergmno[krelg]elr[gpker[gplke]rg
+krnpgkerpgkle]plg]erlg][er
+oertgperkotgportg[rptgle[rgf
+oprktjgmpeorkg[er;gl,

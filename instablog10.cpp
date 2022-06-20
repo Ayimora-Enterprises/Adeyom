bf88826d8238ecc1c4e503a7d28017fc69694e1f
abayomia2015lbr <abayomia.prodacct@gmail.comlbr>
2022-06-20 00:43:00 -0500

    first commit

---
+++ instablog10.cpp
@@ -0,0 +1,13 @@
+During the reign of Alaafin Ajaka, Oyo Empire was under a regular treat of war from Olowu, Ajaka’s cousin; who rules Owu Kingdom. Olowu later sent his warriors to capture Alaafin Ajaka and bring him to Owu. In their bid to rescue Alaafin Ajaka, the Oyomesi sent for Sango.
+
+Sango, after the rescue of Ajaka was crowned King while Alaafin Ajaka was sent into exile. During his reign, he had two generals: Timi Agbale Olofa-ina who could shoot arrows of fire and Gbonka who was equally powerful. Seeing how powerful these two generals grew, Sango followed Oya’s advice to get rid of them and sent them to govern the border towns. Timi left for Ede but Gbonka stayed back in Oyo to pose further treat and in his quest to destroy them both, Sango sent Gbonka to capture Timi.
+
+Sango who believed that the fight between Timi and Gbonka in Ede was staged asked for a re-match in Oyo and Gbonka defeated Timi. Seeing as he couldn’t finish his two threats at once, Sango ordered that Gbonka should be burnt to ashes. This would be in fear of Gbonka finding out that Sango only wanted him dead. Mysteriously, Gbonka appeared after three days giving Sango ultimatum to vacate the throne.
+
+Sango, angered requested for his Edun-Ara from Oya that has being in the possession of it but found it stained with menstrual blood. He was said to have left the palace to a high rock facing the palace in anger to re-affirm the potency of his thunderbolt and the thunder he created that day was said to have struck the palace and burnt it down to ashes. Heartbroken, he left the town followed by the chiefs and members of his royal cult known as Baba-Mogba persuading him not to leave. After a long persuasion, some of the chiefs went back as they approached an Ayan tree in a place called Koso with news that the king has hanged but that is stated to be a lie.
+
+Only a few of the Baba-Mogba who did not go back knew the truth which is that Sango was attacked by Gbonka. Sango unwilling to fight varnished into thin air only to later appear in the sky with wrath and flaming fire to destroy Gbonka. The town peddling the rumor about him hanging himself also got a share of his wrath.
+
+Myths have it that he didn’t stop till he was appeased. Hence the popular saying OBAKOSO or OLUKOSO meaning the king did not hang. As created by the Baba-Mogba who knew the truth. Another faction said the name OBAKOSO or OLUKOSO is derived from the name of the place he won Gbonka to reign supreme.
+
+
